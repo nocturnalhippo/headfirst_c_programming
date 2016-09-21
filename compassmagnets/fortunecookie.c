da0772e6eb465f7bea5c4f5f7185293f08a79dc4
Ronnie Wong <embeddedrw@gmail.com>
2016-09-21 14:39:48 -0400

    added fortunecookie.c

---
+++ compassmagnets/fortunecookie.c
@@ -0,0 +1,16 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+void fortune_cookie(char msg[])
+{
+	printf("Message reads: %s\n",msg);
+}
+
+int main()
+{
+	char quote[] = "Cookies make you fat";
+
+	fortune_cookie(quote);
+
+	return 0;
+}
