534756122182b3e4c83f4c2c94669164b0b199f2
Ronnie Wong <embeddedrw@gmail.com>
2016-10-01 21:52:01 -0400

    while implementation implemented

--- chapter01/code.c
+++ chapter01/code.c
@@ -8,43 +8,53 @@
 int main()
 {
 	char card_name[3];
-	puts("Enter the card_name: ");
-	scanf("%2s",card_name);
-	int val = 0;
+	int count = 0;
 
-	/*
-	if (card_name[0] == 'K') {
-		val = 10;
-	} else if (card_name[0] == 'Q') {
-		val = 10;
-	} else if (card_name[0] == 'J') {
-		val = 10;
-	} else if (card_name[0] == 'A') {
-		val = 11;
-	} else {
-		val = atoi(card_name);
-	}
-	*/
+	while (card_name[0] != 'X'){
 
-	switch(card_name[0]) {
-		case 'K':
-		case 'Q':
-		case 'J':
+		puts("Enter the card_name: ");
+		scanf("%2s",card_name);
+		int val = 0;
+
+		/*
+		if (card_name[0] == 'K') {
 			val = 10;
-			break;
-		case 'A':
+		} else if (card_name[0] == 'Q') {
+			val = 10;
+		} else if (card_name[0] == 'J') {
+			val = 10;
+		} else if (card_name[0] == 'A') {
 			val = 11;
-			break;
-		default:
+		} else {
 			val = atoi(card_name);
-	}
+		}
+		*/
 
-	printf("The card value is: %i\n", val);
+		switch(card_name[0]) {
+			case 'K':
+			case 'Q':
+			case 'J':
+				val = 10;
+				break;
+			case 'A':
+				val = 11;
+				break;
+			case 'X':
+				continue;
+			default:
+				val = atoi(card_name);
+				if (val<1 || val>10){
+					puts("Error. Re-enter a valid number.");
+					continue;
+				}
+		}
 
-	if (val>=3 && val<=6) {
-		puts("Count has gone up");
-	} else if (val == 10) {
-		puts("Count has gone down");
+		if ((val>2) && (val<7)){
+			count++;
+		} else if (val == 10){
+			count--;
+		}
+		printf("Current count: %i\n",count);
 	}
 
 	return 0;
