bf87a175baeea290d8dbfa605fd1ea936bdc23d0
Ronnie Wong <embeddedrw@gmail.com>
2016-10-03 12:15:47 -0400

    corrected jukebox.c to use fgets instead of scanf

--- chapter02/jukebox.c
+++ chapter02/jukebox.c
@@ -31,13 +31,22 @@ void find_track(char search_for[])
 int main()
 {
 	char search_for[80];
+	char new_search_for[80];
 	int i;
+	int len;
 
 	printf("Search for: ");
-	scanf("%79s",search_for);
-	//fgets(search_for, 80, stdin);
+	//scanf("%79s",search_for);
+	fgets(search_for, 80, stdin);
 
-	find_track(search_for);
+	//len = strlen(search_for);
+	//printf("\n%s is of length: %d\n",search_for,len);
+
+	memcpy(new_search_for,search_for,strlen(search_for)-1);
+	len = strlen(new_search_for);
+	printf("\n%s is of length: %d\n",new_search_for,len);
+
+	find_track(new_search_for);
 	
 	return 0;
 }
