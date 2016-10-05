79c6b81cf4239bb9a32d0d2232716eabab0bd478
Ronnie Wong <embeddedrw@gmail.com>
2016-10-04 23:09:06 -0400

    added codemagnet

--- chapter02/codemagnet.c
+++ chapter02/codemagnet.c
@@ -15,6 +15,7 @@ void print_reverse(char *s)
 		t = t - 1;				// decrement -1 and point to next reverse character
 	}							// and repeat till pointer t matches pointer s
 	puts("");
+	printf("\n");
 }
 
 int main()
@@ -22,12 +23,38 @@ int main()
 	char word[80];
 	char toBeReversed[80];
 
+	char *juices[] = {
+		"dragonfruit", "waterberry", "sharonfruit", "uglifruit",
+		"rumberry", "kiwifruit", "mulberry", "strawberry",
+		"blueberry", "blackberry", "starfruit"
+	};
+	char a;
+
+	char *names_for_dog[] = {"Bowser", "Bonza", "Ralph", "Snoopdog"};
+
 	printf("Please enter a string to spell backwards: ");
-	
 	fgets(word,80,stdin);
 	memcpy(toBeReversed,word,strlen(word)-1);
-
 	print_reverse(toBeReversed);
+
+/*
+	puts(juices[6]);
+	print_reverse(juices[7]);
+	a = juices[2];
+	juices[2] = juices[8];
+	juices[8] = a;
+	puts(juices[8]);
+	print_reverse(juices[(18+7) / 5]);
+	puts(juices[2]);
+	print_reverse(juices[9]);
+	juices[1] = juices[3];
+	puts(juices[10]);
+	print_reverse(juices[1]);
+*/
+
+	for(int i = 0; i <= strlen(names_for_dog); i++){
+		printf("Dog %i: %s\n",i,names_for_dog[i]);
+	}
 
 	return 0;
 }
